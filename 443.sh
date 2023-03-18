0733d4d05ac93482e9735fb58b1a2492d5acec88
Hasumiki Yuna <91690580+HasumikiYuna@users.noreply.github.com>
2023-03-18 19:05:53 +0700

    Update 443.sh

--- 443.sh
+++ 443.sh
@@ -113,10 +113,16 @@ rm /var/cache/apt/archives/lock
 pre_install_docker_compose() {
   
 
-  read -p " ID nút (Node_ID):" node_id
+  read -p " ID nút 1 (Node_ID):" node_id
   [ -z "${node_id}" ] && node_id=0
   echo "-------------------------------"
   echo "Node_ID: ${node_id}"
+  echo "-------------------------------"
+  
+  read -p " ID nút 2 (Node_ID2):" node_id2
+  [ -z "${node_id2}" ] && node_id2=0
+  echo "-------------------------------"
+  echo "Node_ID2: ${node_id2}"
   echo "-------------------------------"
 
   read -p " Giới hạn thiết bị (Limit):" limit
@@ -170,6 +176,7 @@ EOF
 }
 EOF
   cat >yunatls.yml <<EOF
+cat >config.yml <<EOF
 Log:
   Level: none # Log level: none, error, warning, info, debug 
   AccessPath: # /etc/XrayR/access.Log
@@ -217,7 +224,7 @@ Nodes:
           ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
       CertConfig:
         CertMode: dns # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
-        CertDomain: "node1.test.com" # Domain to cert
+        CertDomain: "tesst.com" # Domain to cert
         CertFile: ./etc/XrayR/server.pem # Provided if the CertMode is file
         KeyFile: ./etc/XrayR/privkey.pem
         Provider: cloudflare # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
@@ -225,10 +232,51 @@ Nodes:
         DNSEnv: # DNS ENV option used by DNS provider
           CLOUDFLARE_EMAIL: miniwa2509@gmail.com
           CLOUDFLARE_API_KEY: dd6b0ab29d9f421cdad28995cea82593fec61
+  -
+    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
+    ApiConfig:
+      ApiHost: "http://lightspeed4g.pw/"
+      ApiKey: "ultimate1234yuna"
+      NodeID2: 1
+      NodeType: V2ray # Node type: V2ray, Trojan, Shadowsocks, Shadowsocks-Plugin
+      Timeout: 30 # Timeout for the api request
+      EnableVless: false # Enable Vless for V2ray Type
+      EnableXTLS: false # Enable XTLS for V2ray and Trojan
+      SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
+      DeviceLimit: 3 # Local settings will replace remote settings, 0 means disable
+      RuleListPath: # ./rulelist Path to local rulelist file
+    ControllerConfig:
+      ListenIP: 0.0.0.0 # IP address you want to listen
+      SendIP: 0.0.0.0 # IP address you want to send pacakage
+      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
+      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
+      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
+      DisableUploadTraffic: false # Disable Upload Traffic to the panel
+      DisableGetRule: false # Disable Get Rule from the panel
+      DisableIVCheck: false # Disable the anti-reply protection for Shadowsocks
+      EnableProxyProtocol: false # Only works for WebSocket and TCP
+      EnableFallback: false # Only support for Trojan and Vless
+      FallBackConfigs:  # Support multiple fallbacks
+        -
+          SNI: # TLS SNI(Server Name Indication), Empty for any
+          Path: # HTTP PATH, Empty for any
+          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
+          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
+      CertConfig:
+        CertMode: none # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
+        CertDomain: "test.com" # Domain to cert
+        CertFile: /etc/XrayR/server.pem # Provided if the CertMode is file
+        KeyFile: /etc/XrayR/privkey.pem
+        Provider: cloudflare # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
+        Email: test@me.com
+        DNSEnv: # DNS ENV option used by DNS provider
+          CLOUDFLARE_EMAIL: aaa
+          CLOUDFLARE_API_KEY: bbb
 EOF
   
   sed -i "s|NodeID:.*|NodeID: ${node_id}|" ./yunatls.yml
   sed -i "s|DeviceLimit:.*|DeviceLimit: ${limit}|" ./yunatls.yml
+  sed -i "s|NodeID2:.*|NodeID2: ${node_id2}|" ./yunatls.yml
   sed -i "s|CertDomain:.*|CertDomain: ${cerdm}|" ./yunatls.yml
 
 }
